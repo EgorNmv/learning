5dc74246b4a211ba80b439a46df55bb4283d1c8c
Egor Naumov <Egor_Nmv@bk.ru>
2020-06-04 16:33:44 +0500

    added modals to recomendations and reviews

--- packages/front/src/components/TrainingRecommendations/TrainingRecommendations.tsx
+++ packages/front/src/components/TrainingRecommendations/TrainingRecommendations.tsx
@@ -1,11 +1,50 @@
 import React from "react";
-import { Carousel } from "antd";
+import { Carousel, Modal, Button, Input, Form } from "antd";
+import "./TrainingRecomendations.css";
 import { UserCard } from "../UserCard/UserCard";
+import { Store } from "antd/lib/form/interface";
 
 export const TrainingRecommendations: React.FC = () => {
+  const [isVisibleModal, setIsVisibleModal] = React.useState<boolean>(false);
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [form] = Form.useForm();
+
   return (
     <>
-      <h2>Рекомендации</h2>
+      <div className="training-recomendation-title">
+        <h2>Рекомендации</h2>
+        <Button type="link" onClick={() => setIsVisibleModal(true)}>
+          Написать рекомендацию
+        </Button>
+        <Modal
+          closable={false}
+          visible={isVisibleModal}
+          footer={[
+            <Button key="back" onClick={() => setIsVisibleModal(false)}>
+              Отмена
+            </Button>,
+            <Button
+              key="submit"
+              type="primary"
+              loading={isLoading}
+              onClick={() => console.info("Send request")}
+            >
+              Отправить
+            </Button>,
+          ]}
+        >
+          <div />
+          <Form layout={"vertical"} form={form} name="training-create">
+            <Form.Item
+              name="recomendation"
+              label="Рекомендация:"
+              rules={[{ required: true }]}
+            >
+              <Input.TextArea rows={10} />
+            </Form.Item>
+          </Form>
+        </Modal>
+      </div>
       <Carousel>
         <UserCard />
         <UserCard />
--- packages/front/src/components/TrainingReviews/TrainingReviews.tsx
+++ packages/front/src/components/TrainingReviews/TrainingReviews.tsx
@@ -1,10 +1,51 @@
 import React from "react";
 import { UserCard } from "../UserCard/UserCard";
+import { Button, Form, Input, Modal, Rate } from "antd";
 
 export const TrainingReviews: React.FC = () => {
+  const [isVisibleModal, setIsVisibleModal] = React.useState<boolean>(false);
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [form] = Form.useForm();
+
   return (
     <>
-      <h2>Отзывы</h2>
+      <div className="training-recomendation-title">
+        <h2>Отзывы</h2>
+        <Button type="link" onClick={() => setIsVisibleModal(true)}>
+          Написать отзыв
+        </Button>
+        <Modal
+          closable={false}
+          visible={isVisibleModal}
+          footer={[
+            <Button key="back" onClick={() => setIsVisibleModal(false)}>
+              Отмена
+            </Button>,
+            <Button
+              key="submit"
+              type="primary"
+              loading={isLoading}
+              onClick={() => console.info("Send request")}
+            >
+              Отправить
+            </Button>,
+          ]}
+        >
+          <div />
+          <Form layout={"vertical"} form={form} name="training-create">
+            <Form.Item name="raiting" label="Рейтинг">
+              <Rate />
+            </Form.Item>
+            <Form.Item
+              name="review"
+              label="Отзыв:"
+              rules={[{ required: true }]}
+            >
+              <Input.TextArea rows={10} />
+            </Form.Item>
+          </Form>
+        </Modal>
+      </div>
       <UserCard />
       <UserCard />
       <UserCard />
