f68a3d238f7acb190fbe113ef32b027b008e2296
Egor Naumov <Egor_Nmv@bk.ru>
2020-06-05 18:19:02 +0500

    added authForm to Auth page

--- packages/front/src/pages/Auth/Auth.tsx
+++ packages/front/src/pages/Auth/Auth.tsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { CenteredText } from "../../hoc/CenteredText/CenteredText";
+import { Card, Form, Input, Button } from "antd";
+import { Link } from "react-router-dom";
+import "./Auth.css";
 
 const Auth: React.FC = () => {
-  return <div>Auth page</div>;
+  return (
+    <section>
+      <CenteredText className="auth-page-main-form">
+        <Card>
+          <h1>Авторизоваться</h1>
+          <Form
+            name="auth-form"
+            onFinish={() => console.info("onFinish")}
+            onFinishFailed={() => console.info("onFinishFailed")}
+          >
+            <Form.Item
+              label="Ваш логин:"
+              name="login"
+              rules={[{ required: true, message: "Введите ваш логин" }]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Ваш пароль:"
+              name="password"
+              rules={[{ required: true, message: "Введите пароль" }]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Войти
+              </Button>
+            </Form.Item>
+          </Form>
+          <span>
+            Нет аккаунта? <Link to="/register">Зарегистрироваться</Link>
+          </span>
+        </Card>
+      </CenteredText>
+    </section>
+  );
 };
 
 export default Auth;
