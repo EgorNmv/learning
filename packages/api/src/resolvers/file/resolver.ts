6ed25afab3b336430fd22f3b414fba9312d3a50b
Egor Naumov <Egor_Nmv@bk.ru>
2020-06-10 15:07:40 +0500

    changed file resolver

--- packages/api/src/resolvers/file/resolver.ts
+++ packages/api/src/resolvers/file/resolver.ts
@@ -8,17 +8,22 @@ export class FileUploadResolver {
     public async uploadFile(
         @Arg("file", type => GraphQLUpload) file: FileUpload
     ): Promise<boolean> {
+        if (!file) {
+            return false;
+        }
+        console.info(file);
+        return true;
 
-        const { createReadStream, filename } = await file;
-        const writableStream = createWriteStream(
-            `${__dirname}/../../../files/images/${filename}`,
-            { autoClose: true }
-        );
-        return await new Promise((res, rej) => {
-            createReadStream()
-                .pipe(writableStream)
-                .on("finish", () => res(true))
-                .on("error", () => rej(false));
-        });
+        // const { createReadStream, filename } = await file;
+        // const writableStream = createWriteStream(
+        //     `${__dirname}/../../../files/images/${filename}`,
+        //     { autoClose: true }
+        // );
+        // return await new Promise((res, rej) => {
+        //     createReadStream()
+        //         .pipe(writableStream)
+        //         .on("finish", () => res(true))
+        //         .on("error", () => rej(false));
+        // });
     }
 }
