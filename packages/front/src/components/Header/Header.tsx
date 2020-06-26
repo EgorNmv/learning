61440e5da5af63e6c35b4eb57b23aba5413e2a4d
Наумов Е.А <65NaumovEA@d65201700.DEV.URAL>
2020-06-26 11:23:27 +0500

    added hidden sider and header

--- packages/front/src/components/Header/Header.tsx
+++ packages/front/src/components/Header/Header.tsx
@@ -3,14 +3,22 @@ import { Layout, Select } from "antd";
 import { SearchableInput } from "../SearchableInput/SearchableInput";
 import "./Header.css";
 import { UserMenuWithLinks } from "../UserMenuWithLinks/UserMenuWithLinks";
+import { useOktaAuth } from "@okta/okta-react";
 
-export const Header: React.FC = () => (
-  <Layout.Header className="all-main-header">
-    <div>
-      <SearchableInput />
-    </div>
-    <div className="all-main-header-user-links">
-      <UserMenuWithLinks />
-    </div>
-  </Layout.Header>
-);
+export const Header: React.FC = () => {
+  const { authState } = useOktaAuth();
+  return (
+    <Layout.Header className="all-main-header">
+      {authState.isAuthenticated
+        && <div>
+          <SearchableInput />
+        </div>
+      }
+      {authState.isAuthenticated
+        && <div className="all-main-header-user-links">
+          {authState.isAuthenticated && <UserMenuWithLinks />}
+        </div>
+      }
+    </Layout.Header>
+  );
+}
--- packages/front/src/components/Sider/Sider.tsx
+++ packages/front/src/components/Sider/Sider.tsx
@@ -3,19 +3,26 @@ import "./Sider.css";
 import { Layout } from "antd";
 import { SiderLinkList } from "../SiderLinkList/SiderLinkList";
 import { constants } from "../../constants/constants";
+import { useOktaAuth } from "@okta/okta-react";
 
-export const Sider: React.FC = () => (
-  <Layout.Sider className="all-sider-layout">
-    <div className="sider-content">
-      <div>
-        <div className="sider-title-circle"></div>
-        <div className="sider-title-text">
-          <span>{constants["BANK"]}</span>
+export const Sider: React.FC = () => {
+  const { authState } = useOktaAuth();
+
+  return (
+    <Layout.Sider className="all-sider-layout">
+      <div className="sider-content">
+        <div>
+          <div className="sider-title-circle"></div>
+          <div className="sider-title-text">
+            <span>{constants["BANK"]}</span>
+          </div>
         </div>
+        {authState.isAuthenticated
+          && <div>
+            <SiderLinkList />
+          </div>
+        }
       </div>
-      <div>
-        <SiderLinkList />
-      </div>
-    </div>
-  </Layout.Sider>
-);
+    </Layout.Sider>
+  )
+};
