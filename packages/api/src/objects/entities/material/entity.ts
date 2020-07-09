ca4819f9557c8b4bd92b080a4c3681dc4976e612
Наумов Е.А <65NaumovEA@d65201700.DEV.URAL>
2020-07-09 14:08:07 +0500

    fixed material relation

--- packages/api/src/objects/entities/material/entity.ts
+++ packages/api/src/objects/entities/material/entity.ts
@@ -1,47 +1,51 @@
 import { ObjectType, Field } from "type-graphql";
 import { TrainingEntity } from "../training/entity";
-import { Entity, BaseEntity, PrimaryGeneratedColumn, OneToOne, JoinColumn, Column, RelationId } from "typeorm";
+import {
+  Entity,
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  JoinColumn,
+  Column,
+  RelationId,
+  ManyToOne,
+} from "typeorm";
 import { Material } from "./type";
 
 @Entity({ name: "Material" })
 @ObjectType({
-    description: "Материалы обучения"
+  description: "Материалы обучения",
 })
 export class MaterialEntity extends BaseEntity implements Material {
-    @Field(() => Number, {
-        nullable: false,
-        description: "id материала"
-    })
-    @PrimaryGeneratedColumn()
-    public id: number;
+  @Field(() => Number, {
+    nullable: false,
+    description: "id материала",
+  })
+  @PrimaryGeneratedColumn()
+  public id: number;
 
-    @Field(() => TrainingEntity, {
-        nullable: false,
-        description: "идентификатор обучения"
-    })
-    @OneToOne(type => TrainingEntity, training => training.id)
-    @JoinColumn()
-    public training: TrainingEntity;
+  @Field(() => TrainingEntity, {
+    nullable: false,
+    description: "идентификатор обучения",
+  })
+  @ManyToOne((type) => TrainingEntity, (training) => training.id)
+  @JoinColumn()
+  public training: TrainingEntity;
 
-    @Column()
-    @RelationId((material: MaterialEntity) => material.training)
-    public trainingId: number;
+  @Column()
+  @RelationId((material: MaterialEntity) => material.training)
+  public trainingId: number;
 
-    @Field(() => String, {
-        nullable: false,
-        description: "ссылка на материалы в файловом хранилище"
-    })
-    @Column()
-    public link: string;
+  @Field(() => String, {
+    nullable: false,
+    description: "ссылка на материалы в файловом хранилище",
+  })
+  @Column()
+  public link: string;
 
-    constructor(
-        id: number,
-        training: TrainingEntity,
-        link: string
-    ) {
-        super();
-        this.id = id;
-        this.training = training;
-        this.link = link;
-    }
+  constructor(id: number, training: TrainingEntity, link: string) {
+    super();
+    this.id = id;
+    this.training = training;
+    this.link = link;
+  }
 }
--- packages/api/src/resolvers/material/queries/createMaterial.ts
+++ packages/api/src/resolvers/material/queries/createMaterial.ts
@@ -1,16 +1,16 @@
 import { MaterialEntity } from "../../../objects/entities/material/entity";
 import { Connection } from "typeorm";
-import { getLocallyConnection } from "../../../../../core/src/database-connection/database-connection";
 import { InputMaterial } from "../../../objects/input-objects/inputMaterial";
 import { findMaterialById } from "./findMaterialById";
 
 export const createMaterial = async (
-    connection: Connection,
-    data: InputMaterial
+  connection: Connection,
+  data: InputMaterial
 ): Promise<MaterialEntity> => {
-    const { id }: MaterialEntity = await connection.getRepository(MaterialEntity)
-        .save({ ...data });
-    const newMaterial: MaterialEntity = await findMaterialById(connection, id);
+  const { id }: MaterialEntity = await connection
+    .getRepository(MaterialEntity)
+    .save({ ...data });
+  const newMaterial: MaterialEntity = await findMaterialById(connection, id);
 
-    return newMaterial;
-}
+  return newMaterial;
+};
