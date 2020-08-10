6c4e89a514a14411fce84beabac8ccf90d89827e
Наумов Е.А <65NaumovEA@d65201700.DEV.URAL>
2020-08-10 16:52:40 +0500

    fixed error if feedback entity are empty

--- packages/api/src/resolvers/training/queries/findAllSortedTrainings.ts
+++ packages/api/src/resolvers/training/queries/findAllSortedTrainings.ts
@@ -10,65 +10,77 @@ export const findAllSortedTrainings = async (
   categoryId: number
 ): Promise<TrainingEntity[]> => {
   if (sortBy === "recommends") {
-    const trainingIdsWithRecommends: FeedbackEntity[] = await connection
-      .getRepository(FeedbackEntity)
-      .find({ select: ["trainingId"] });
-    const trainingIdsWithCountOfFeedbacks: {
-      [trainigId: string]: number;
-    } = trainingIdsWithRecommends.reduce((acc, cur) => {
-      if (acc[`${cur.trainingId}`]) {
-        acc[`${cur.trainingId}`] += 1;
+    try {
+      const trainingIdsWithRecommends: FeedbackEntity[] = await connection
+        .getRepository(FeedbackEntity)
+        .find({ select: ["trainingId"] });
+      const trainingIdsWithCountOfFeedbacks: {
+        [trainigId: string]: number;
+      } = trainingIdsWithRecommends.reduce((acc, cur) => {
+        if (acc[`${cur.trainingId}`]) {
+          acc[`${cur.trainingId}`] += 1;
 
-        return acc;
-      } else {
-        acc[`${cur.trainingId}`] = 1;
-
-        return acc;
-      }
-    }, {});
-
-    const sortedListOfIds: number[] = Object.keys(
-      trainingIdsWithCountOfFeedbacks
-    )
-      .sort((a, b) => {
-        if (sortOrder === "ASC") {
-          return (
-            trainingIdsWithCountOfFeedbacks[a] -
-            trainingIdsWithCountOfFeedbacks[b]
-          );
+          return acc;
         } else {
-          return (
-            trainingIdsWithCountOfFeedbacks[b] -
-            trainingIdsWithCountOfFeedbacks[a]
-          );
-        }
-      })
-      .map(Number);
-    const trainings: TrainingEntity[] = await connection
-      .getRepository(TrainingEntity)
-      .find({
-        relations: ["format", "organizer", "audience", "category"],
-        where: { id: In([...sortedListOfIds]), categoryId },
-      });
-    const trainingsWithoutAnyRecommends: TrainingEntity[] = await connection
-      .getRepository(TrainingEntity)
-      .find({
-        relations: ["format", "organizer", "audience", "category"],
-        where: { id: Not(In([...sortedListOfIds])), categoryId },
-      });
-    const sortedObjectOfIds: {
-      [trainingId: string]: number;
-    } = sortedListOfIds.reduce((acc, cur, index) => {
-      acc[`${cur}`] = index;
-      return acc;
-    }, {});
-    const sortedTrainingByFeedbacks: TrainingEntity[] = trainings.sort(
-      (a, b) => sortedObjectOfIds[a.id] - sortedObjectOfIds[b.id]
-    );
+          acc[`${cur.trainingId}`] = 1;
 
-    return sortOrder === "ASC"
-      ? [...trainingsWithoutAnyRecommends, ...sortedTrainingByFeedbacks]
-      : [...sortedTrainingByFeedbacks, ...trainingsWithoutAnyRecommends];
+          return acc;
+        }
+      }, {});
+
+      const sortedListOfIds: number[] = Object.keys(
+        trainingIdsWithCountOfFeedbacks
+      )
+        .sort((a, b) => {
+          if (sortOrder === "ASC") {
+            return (
+              trainingIdsWithCountOfFeedbacks[a] -
+              trainingIdsWithCountOfFeedbacks[b]
+            );
+          } else {
+            return (
+              trainingIdsWithCountOfFeedbacks[b] -
+              trainingIdsWithCountOfFeedbacks[a]
+            );
+          }
+        })
+        .map(Number);
+      const trainings: TrainingEntity[] = await connection
+        .getRepository(TrainingEntity)
+        .find({
+          relations: ["format", "organizer", "audience", "category"],
+          where: { id: In([...sortedListOfIds]), categoryId },
+        });
+      const trainingsWithoutAnyRecommends: TrainingEntity[] = await connection
+        .getRepository(TrainingEntity)
+        .find({
+          relations: ["format", "organizer", "audience", "category"],
+          where: { id: Not(In([...sortedListOfIds])), categoryId },
+        });
+      const sortedObjectOfIds: {
+        [trainingId: string]: number;
+      } = sortedListOfIds.reduce((acc, cur, index) => {
+        acc[`${cur}`] = index;
+        return acc;
+      }, {});
+      const sortedTrainingByFeedbacks: TrainingEntity[] = trainings.sort(
+        (a, b) => sortedObjectOfIds[a.id] - sortedObjectOfIds[b.id]
+      );
+
+      return sortOrder === "ASC"
+        ? [...trainingsWithoutAnyRecommends, ...sortedTrainingByFeedbacks]
+        : [...sortedTrainingByFeedbacks, ...trainingsWithoutAnyRecommends];
+    } catch (error) {
+      console.error("Error in findAllSortedTrainings", error);
+
+      const trainingsInThisCategory: TrainingEntity[] = await connection
+        .getRepository(TrainingEntity)
+        .find({
+          where: { categoryId },
+        });
+
+      return trainingsInThisCategory;
+    }
   } else {
     const trainings: TrainingEntity[] = await connection
       .getRepository(TrainingEntity)
