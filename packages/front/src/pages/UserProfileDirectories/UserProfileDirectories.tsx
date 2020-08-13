c634e6cf622fa4629621cf14025a1eeefc6c47bc
Наумов Е.А <65NaumovEA@d65201700.DEV.URAL>
2020-08-13 21:55:26 +0500

    removed users link in directories list

--- packages/front/src/pages/UserProfileDirectories/UserProfileDirectories.tsx
+++ packages/front/src/pages/UserProfileDirectories/UserProfileDirectories.tsx
@@ -19,28 +19,27 @@ const UserProfileDirectories: React.FC = () => {
     },
   ];
   const data = [
-    { key: 1, id: 1, name: "Пользователи", link: "/profile/directories/users" },
     {
-      key: 2,
-      id: 2,
+      key: 1,
+      id: 1,
       name: "Категории",
       link: "/profile/directories/categories",
     },
     {
-      key: 3,
-      id: 3,
+      key: 2,
+      id: 2,
       name: "Организаторы",
       link: "/profile/directories/organizers",
     },
     {
-      key: 4,
-      id: 4,
+      key: 3,
+      id: 3,
       name: "Целевая аудитория",
       link: "/profile/directories/targetaudiences",
     },
     {
-      key: 5,
-      id: 5,
+      key: 4,
+      id: 4,
       name: "Формат обучения",
       link: "/profile/directories/trainingformats",
     },
