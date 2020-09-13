746ae5c7f646c1ed035c41f206553109facf1b43
EgorNmv <43643833+EgorNmv@users.noreply.github.com>
2020-09-14 01:11:13 +0500

    added styles to user profile directories page

--- packages/front/src/pages/UserProfileDirectories/UserProfileDirectories.tsx
+++ packages/front/src/pages/UserProfileDirectories/UserProfileDirectories.tsx
@@ -1,25 +1,36 @@
 import React from "react";
-import { Card, Table, Button } from "antd";
+import { Card, Table } from "antd";
 import { Link } from "react-router-dom";
 import { Breadcrumbs } from "../../components/Breadcrumbs/Breadcrumbs";
+import { ColumnsType } from "antd/es/table";
+import "./user-profile-directories.css";
+
+type Directory = {
+  key: number;
+  id: number;
+  name: string;
+  link: string;
+};
 
 const UserProfileDirectories: React.FC = () => {
-  const columns = [
+  const columns: ColumnsType<Directory> = [
     {
       title: "№",
       dataIndex: "id",
+      align: "center",
+      width: "5rem"
     },
     {
       title: "Название",
       dataIndex: "name",
-      render: (text: string, record: any) => (
-        <Button type="link">
-          <Link to={record.link}>{text}</Link>
-        </Button>
+      render: (text, record) => (
+        <Link className="directories-table__link" to={record.link}>
+          {text}
+        </Link>
       ),
     },
   ];
-  const data = [
+  const data: Directory[] = [
     {
       key: 1,
       id: 1,
@@ -47,17 +58,22 @@ const UserProfileDirectories: React.FC = () => {
   ];
 
   return (
-    <section>
+    <section className="user-directories">
       <Breadcrumbs />
-      <span>
-        <h1>Справочники</h1>
-      </span>
-      <Card>
-        <Table
+      <h2>Справочники</h2>
+      <Card className="directories-table__card">
+        <Table<Directory>
+          className="directories-table"
           bordered
           columns={columns}
           dataSource={data}
+          onHeaderRow={(column) => {
+            return {
+              className: "directories-table__header",
+            };
+          }}
           pagination={false}
+          rowClassName="directories-table__row"
         />
       </Card>
     </section>
