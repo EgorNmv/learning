04285a2a6023efe9c7beb8fcbec5bdddcea9825f
Наумов Е.А <65NaumovEA@d65201700.DEV.URAL>
2020-09-17 18:34:03 +0500

    changed newTraining query

--- packages/api/src/resolvers/training/queries/findAllNewTrainings.ts
+++ packages/api/src/resolvers/training/queries/findAllNewTrainings.ts
@@ -2,23 +2,18 @@ import { Connection } from "typeorm";
 import { TrainingEntity } from "../../../objects/entities/training/entity";
 
 export const findAllNewTrainings = async (
-    connection: Connection
+  connection: Connection
 ): Promise<TrainingEntity[]> => {
-    const trainings: TrainingEntity[] = await connection.getRepository(TrainingEntity).find({
-        relations: ["format", "organizer", "audience", "category"]
+  const trainings: TrainingEntity[] = await connection
+    .getRepository(TrainingEntity)
+    .find({
+      relations: ["format", "organizer", "audience", "category"],
     });
-    /**
-     *  Сортировка массива по дате создания курса, дата в формате "22.03.20", самые новые -> начало массива
-     */
-    const sortedTrainings: TrainingEntity[] = trainings.sort(
-        (trainingA, trainingB) => {
-            const [ddA, mmA, yyA]: number[] = trainingA.createDate.split(".").map(Number);
-            const dateA: Date = new Date(Number(`20${yyA}`), mmA, ddA);
-            const [ddB, mmB, yyB]: number[] = trainingB.createDate.split(".").map(Number);
-            const dateB: Date = new Date(Number(`20${yyB}`), mmB, ddB);
+  const sortedTrainings: TrainingEntity[] = trainings
+    .sort((trainingA, trainingB) => {
+      return trainingB.createDate.getTime() - trainingA.createDate.getTime();
+    })
+    .slice(0, 19);
 
-            return <any>dateB - <any>dateA;
-        });
-
-    return sortedTrainings;
-}
+  return sortedTrainings;
+};
