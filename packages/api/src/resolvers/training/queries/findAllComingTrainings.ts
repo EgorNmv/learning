aa48a3a14a378e7638e94086ea7b871aa8f84444
Наумов Е.А <65NaumovEA@d65201700.DEV.URAL>
2020-09-17 18:15:10 +0500

    fixed comingTraining query

--- packages/api/src/resolvers/training/queries/findAllComingTrainings.ts
+++ packages/api/src/resolvers/training/queries/findAllComingTrainings.ts
@@ -11,21 +11,25 @@ export const findAllComingTrainings = async (
       where: { isDateSet: true },
     });
   const currentDate: Date = new Date();
-  const sortedTrainings = trainings
+
+  currentDate.setHours(0, 0, 0, 0);
+
+  const sortedTrainings: TrainingEntity[] = trainings
     .filter((training) => {
       const [dd, mm, yy]: number[] = training.start.split(".").map(Number);
-      const trainingStartDate: Date = new Date(Number(`20${yy}`), mm - 1, dd);
+      const trainingStartDate: Date = new Date(yy, mm - 1, dd);
 
       return currentDate <= trainingStartDate;
     })
     .sort((trainingA, trainingB) => {
       const [ddA, mmA, yyA]: number[] = trainingA.start.split(".").map(Number);
-      const dateA: Date = new Date(Number(`20${yyA}`), mmA, ddA);
+      const dateA: Date = new Date(yyA, mmA, ddA);
       const [ddB, mmB, yyB]: number[] = trainingB.start.split(".").map(Number);
-      const dateB: Date = new Date(Number(`20${yyB}`), mmB, ddB);
+      const dateB: Date = new Date(yyB, mmB, ddB);
 
-      return <any>dateA - <any>dateB;
-    });
+      return dateA.getTime() - dateB.getTime();
+    })
+    .slice(0, 19);
 
   return sortedTrainings;
 };
